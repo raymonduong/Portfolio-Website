c48f6f6913987c7eaedb187583ff3cf43911265c
Raymond <raymonduong@gmail.com>
2021-12-23 23:23:50 -0700

    Finalization of the website

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-var time = new Date();
-var hours = time.getHours();
-var greet;
-if (hours < 12) greet = "Good morning";
-else if (hours >= 12 && hours <= 17) greet = "Good afternoon";
-else if (hours >= 17 && hours <= 24) greet = "Good evening";
-document.getElementById("Greetings").innerHTML =
-  greet + ", my name is Raymond. 👋";
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -25,3 +16,11 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
+var time = new Date();
+var hours = time.getHours();
+var greet;
+if (hours < 12) greet = "Good morning";
+else if (hours >= 12 && hours <= 17) greet = "Good afternoon";
+else if (hours >= 17 && hours <= 24) greet = "Good evening";
+document.getElementById("Greetings").innerHTML =
+  greet + ", my name is Raymond. 👋";
